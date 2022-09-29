ccd670d517aeba58d840e194b9a959f077665819
Olga-cyber84 <doorsdream007@ya.ru>
2022-09-29 13:10:03 +0300

    first corrections

--- main_test.py
+++ main_test.py
@@ -1,13 +1,7 @@
 import requests
-from pprint import pprint
 import json
 import urllib.request
-
-access_token = ''
-with open('token.txt', 'r') as f:
-    access_token = f.read().strip()
-
-user_id = '253472352'
+from datetime import datetime
 
 
 class VK:
@@ -17,86 +11,130 @@ class VK:
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
 
-    def users_info(self):
+    def user_info(self):
         url = 'https://api.vk.com/method/photos.get'
         params = {'owner_ids': self.id, 'album_id': 'profile',
                   'extended': 1, 'photo_sizes': 1}
         response = requests.get(url, params={**self.params, **params})
         return response.json()
 
-    def get_max_photo(self, user_avatars):
+    def get_maxsize_photo(self, user_avatars):
+        """Метод определяет фото макимального размера, формируем список их url-адресов для выгрузки"""
         avatars = user_avatars['response']['items']
         data_to_file = []
-        photo_url_list = []
-        for item, photo in enumerate(avatars):
-            photo_likes = photo['likes']['count']
-            photo_date = photo['date']
+        photo_list = []
+        photo_likes_set = set()
+        photo_likes_list = []
+
+        for ava in avatars:
+            photo_likes = ava['likes']['count']
+            photo_likes_set.add(photo_likes)
+            photo_likes_list.append(photo_likes)
+            photo_date = ava['date']
             max_height = 0
             max_width = 0
             photo_url = ''
 
-            for photo_params in photo['sizes']:
+            for photo_params in ava['sizes']:
                 if photo_params['height'] > max_height:
                     max_height = photo_params['height']
                     max_width = photo_params['width']
                     photo_url = photo_params['url']
-            photo_url_list.append(photo_url)
 
-            url = photo_url
-            response = urllib.request.urlopen(url)
-            data = response.read()
-
-            with open(f'avatar_{item+1}.jpg', 'wb') as file_object:
-                file_object.write(data)
-            data_to_file.append({
-                "file_name": photo_url.split('/')[7].split('?')[0],
-                "size": max_height,
-                "height": max_height,
-                "width": max_width
+            # формируем список фото с кол-во лайков, датой и url-адресами для выгрузки
+            photo_list.append({
+                'photo_likes': photo_likes,
+                'photo_data': photo_date,
+                'photo_url': photo_url
             })
+        # формируем название и скачиваем файлы на комп
+        photo_name = ''
+        photo_to_ydisk = []
+        for photo in photo_list:
+            response = urllib.request.urlopen(photo['photo_url'])
+            photo_downloaded = response.read()
+            if len(photo_likes_set) == len(photo_likes_list):
+                photo_name = f"{photo['photo_likes']}.jpg"
+            else:
+                photo_name = f"{photo['photo_likes']}_{photo['photo_date']}.jpg"
+            with open(f"{photo_name}", 'wb') as file_object:
+                file_object.write(photo_downloaded)
+            photo_to_ydisk.append({
+                'photo_name': photo_name,
+                'photo_url': photo['photo_url']
+            })
+
+        # формируем данные для файла json)
+        data_to_file.append({
+            "file_name": photo_url.split('/')[7].split('?')[0],
+            "size": max_height,
+            "height": max_height,
+            "width": max_width
+        })
         with open('photos.json', 'w') as f:
             json.dump(data_to_file, f)
-        return photo_url_list
+        return photo_to_ydisk
 
 
 class YaUploader:
+    """Метод загружает файл на яндекс диск"""
+
     def __init__(self, token: str):
         self.token = token
 
     def create_dir(self, dir_name):
+        print(dir_name)
         url = f"https://cloud-api.yandex.net/v1/disk/resources?path={dir_name}"
         headers = {'Content-Type': 'application/json',
                    'Accept': 'application/json', 'Authorization': f'OAuth {self.token}'}
         response = requests.put(url, headers=headers)
         dir = response.json()
-        print(dir)
 
-    def upload(self, dir_name, photos):
+    def upload(self, dir_name, photos, photos_default=5):
         """Метод загружает файл на яндекс диск"""
         headers = {'Content-Type': 'application/json',
                    'Accept': 'application/json', 'Authorization': f'OAuth {self.token}'}
-        photo_quantity_default = 5
-        if len(photos) > 5:
-            photos_default = photos[0:5]
+        # фиксируем кол-во фото
+        if len(photos) > photos_default:
+            photos_default = photos[0:photos_default]
         else:
             photos_default = photos
-        for item in range(len(photos_default)):
+        print(dir_name)
+        for photo in photos_default:
             URL = "https://cloud-api.yandex.net/v1/disk/resources"
+            response = requests.get(
+                f"{URL}/upload?path={dir_name}/{photo['photo_name']}", headers=headers)
+            print("--> ", response.json())
+            url_for_loading = response.json()["href"]
+            message = ''
 
-            with open(photos_default[item], 'wb') as f:
-                response = requests.post(
-                    f'{URL}/upload?path={dir_name}/avatar_{int(item)+1}&url={photos_default[item]}', headers=headers, files={"file": f})
-
-                print(response.json())
+            with open(str(photo['photo_name']), 'rb') as f:
+                response = requests.put(
+                    url_for_loading, headers=headers, files={"file": f})
+                if response.status_code == 201:
+                    message = f"File {photo['photo_name']} downloaded successfully"
+                else:
+                    message = f"File {photo['photo_name']} not downloaded"
+            # логирование загрузки фото
+            with open('logging.txt', 'a') as log_f:
+                log_f.write(
+                    f"{str(datetime.now()).split('.')[0]} - {message}\n")
 
 
 if __name__ == '__main__':
-    vk = VK(access_token, user_id)
-    response_info = vk.users_info()
-    max_photos = vk.get_max_photo(response_info)
+    access_vk_token = ''
+    access_ydisk_token = ''
 
-    dir_name = "avatar_vk"
-    token = "y0_AgAAAAAiqXYTAADLWwAAAADOBUkiVF9IpvSKR963QnUj16X92F_-LHE"
-    uploader = YaUploader(token)
-    # uploader.create_dir(dir_name)
+    with open('token.txt', 'r') as f:
+        access_vk_token = f.readline().strip()
+        access_ydisk_token = f.readline().strip()
+
+    USER_ID = '253472352'
+    vk = VK(access_vk_token, USER_ID)
+    response_info = vk.user_info()
+    max_photos = vk.get_maxsize_photo(response_info)
+
+    dir_name = f"avatar_vk_{str(datetime.now()).split('.')[0].replace(':','_')}"
+    uploader = YaUploader(access_ydisk_token)
+    uploader.create_dir(dir_name)
     result = uploader.upload(dir_name, max_photos)
